e1fbf8db2fd31018652a1feccecae9e8b6f66ff2
SyedZeeshanAli12 <syedzeeshanali877@gmail.com>
2023-07-05 16:11:52 +0500

    welcome page settings

--- cypress/e2e/Welcome.cy.js
+++ cypress/e2e/Welcome.cy.js
@@ -2,32 +2,22 @@ import SignUp from "../POM/SignUp"
 import WelcomePage from "../POM/WelcomePage"
 
 const signup = new SignUp()
+
 const welcomepage = new WelcomePage()
 
-// describe('Sign up page test', () => {
-//     beforeEach(()=>{
-//         signup.navigate('https://www.demoblaze.com/index.html')
-//     })
-//     it('SignupForm', () => {
-//        signup.SignUp()
-//        cy.wait(2000)
-//        signup.Username('zeeshan ali')
-//        signup.Password('shan532029')
-//        signup.Submit()
-//   })
-// })
-
-
-
-  it('Welcome Page', () => {
-    // cy.loginSession;
-    // cy.wait(1000)
-    // welcomepage.movecarouselforward()
-    // cy.wait(2000)
-    // welcomepage.movecarouselbackward()
-    // cy.wait(2000)
-    // welcomepage.carouselindicator()
-    // cy.wait(2000)
+describe('Welcome page test', () => {
+    beforeEach(()=>{
+        cy.login('zeeshan ali', 'shan532029')
+    })
+   it('Welcome Page', () => {
+    cy.loginSession;
+    cy.wait(1000)
+    welcomepage.movecarouselforward()
+    cy.wait(2000)
+    welcomepage.movecarouselbackward()
+    cy.wait(2000)
+    welcomepage.carouselindicator()
+    cy.wait(2000)
     welcomepage.NextPage()
     cy.wait(2000)
     welcomepage.PreviousPage()
@@ -38,4 +28,9 @@ const welcomepage = new WelcomePage()
     cy.wait(2000)
     welcomepage.PCcategory()
     cy.wait(2000)
+  })
 })
+
+
+
+ 
